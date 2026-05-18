49ca8f8c06a2bc1246a3d818455da72727cdbfe6
Sedighe Raeisi <sedighe.raeisi@gmail.com>
2026-05-18 10:33:47 +0200

    LOCAL: traj plot switched to sindy_PI2

--- examples/LV_SINDyPI/noisy_traj_plot.py
+++ examples/LV_SINDyPI/noisy_traj_plot.py
@@ -4,10 +4,10 @@ from src.plot import plot_check_stationary
 import os
 root_path = os.getcwd()
 
-save_dir_prefix = "LVM_SINDyPI_" # Changed prefix for Lotka-Volterra checkpoints
+save_dir_prefix = "LVM_SINDyPI2_" # Changed prefix for Lotka-Volterra checkpoints
 
 true_params_file_str = f"chk_GT_Data.pkl" # Updated filename
 save_path = os.path.join(root_path, [file for file in os.listdir(root_path) if file.startswith(save_dir_prefix)][0])
 print(save_path)
 # plot_all_traj(save_path,gt_utils,realparame2gtarray, true_params_file_str,n_ind=5)
-feature_plot(save_path,gt_utils,realparame2gtarray, true_params_file_str,n_ind=4,stop_subplot_n=None,scaler=None) # scaler = "min-max" or "z-score"
+feature_plot(save_path,gt_utils,realparame2gtarray, true_params_file_str,n_ind=4,stop_subplot_n=None,scaler="z-score") # scaler = "min-max" or "z-score"
